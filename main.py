d7d21bf5ba25bef6c74b50f301c9b6f225182a30
itzsenan <senanpduffy@gmail.com>
2026-08-26 19:39:40 +0100

    Added README.md, added exit mode and optimised functions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
+import math
 
 # Calculates optimal guess
 def calculate_optimal(highest_guess: int, lowest_guess: int) -> int:
-    return round(((highest_guess + lowest_guess) / 2), 0)
+    return math.floor((highest_guess + lowest_guess) / 2)
 
 # Game function
 def guessing_game(tell_optimal: bool):
@@ -24,8 +25,8 @@ def guessing_game(tell_optimal: bool):
 
             guess = input("Guess a number. [1-100]:\n")
             try:
-                intGuess = int(guess)
-                if intGuess > 100 or intGuess < 1:
+                int_guess = int(guess)
+                if int_guess > 100 or int_guess < 1:
                         print("That number is not in the range!")
                 else:
                     guessing = False
@@ -36,18 +37,16 @@ def guessing_game(tell_optimal: bool):
                     exit()
                 print("That's not a number!")
 
-        if intGuess == rand_int:
+        if int_guess == rand_int:
             print(f"You have guessed the correct number of {rand_int} in {guesses}!")
             guessed = True
-        elif intGuess < rand_int:
+        elif int_guess < rand_int:
             print("Your guess is lower than the number!")
-            if intGuess > lowest_guess:
-                lowest_guess = intGuess
+            lowest_guess = max(lowest_guess, int_guess)
         else:
             print("Your guess is higher than the number!")
-            if intGuess < highest_guess:
-                highest_guess = intGuess
-            
+            highest_guess = min(highest_guess, int_guess)
+
         print("-------------------------------")
 
 # Starting variables
@@ -56,7 +55,7 @@ guessing = False
 guessed = False
 
 while playing:
-    answer = input("Do you want to play? [Y/n]:\n").lower()
+    answer = input("Do you want to play? [Y/n] ").lower()
     if answer == "n":
         playing = False
     elif answer == "o": # O for optimal
